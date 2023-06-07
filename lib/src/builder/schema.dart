2478f8324f1cc4b8716f11e305ed9e8b08219595
MinhNguyen <minhnguyen@MinhNguyens-MacBook-Pro.local>
2023-06-08 03:03:19 +0700

    update generate schema logic

--- lib/src/builder/schema.dart
+++ lib/src/builder/schema.dart
@@ -14,6 +14,7 @@ class SchemaState {
 
   Map<Element, TableElement> get tables =>
       _assets.values.map((a) => a.tables).fold({}, (a, b) => a..addAll(b));
+
   Map<String, JoinTableElement> get joinTables =>
       _assets.values.map((a) => a.joinTables).fold({}, (a, b) => a..addAll(b));
 
@@ -23,18 +24,39 @@ class SchemaState {
 
   AssetState createForAsset(AssetId assetId) {
     assert(!_didFinalize, 'Schema was already finalized.');
-    var asset = AssetState(p.basename(assetId.path));
+    var asset = AssetState(p.basename(assetId.path), assetId.path);
     return _assets[assetId] = asset;
   }
 
   AssetState? getForAsset(AssetId assetId) {
-    finalize();
+    final path = _assets[assetId]?.filePath;
+    if (path == assetId.path) {
+      action(_assets[assetId]?.tables);
+    }
     return _assets[assetId];
   }
 
+  void action(Map<Element, TableElement>? tables) {
+    if (tables == null) return;
+    for (var element in tables.values) {
+      element.prepareColumns();
+    }
+    for (var element in tables.values) {
+      element.sortColumns();
+    }
+    for (var element in tables.values) {
+      element.analyzeViews();
+    }
+  }
+/*
   void finalize() {
+    tables.forEach((key, value) {
+      print("table name: ${value.tableName}");
+    });
+
     if (!_didFinalize) {
       for (var element in tables.values) {
+        print("---------> do finazlize ${element.tableName}");
         element.prepareColumns();
       }
       for (var element in tables.values) {
@@ -45,16 +67,17 @@ class SchemaState {
       }
       _didFinalize = true;
     }
-  }
+  }*/
 }
 
 class AssetState {
   final String filename;
+  final String filePath;
 
   Map<Element, TableElement> tables = {};
   Map<String, JoinTableElement> joinTables = {};
 
-  AssetState(this.filename);
+  AssetState(this.filename, this.filePath);
 }
 
 class BuilderState {
